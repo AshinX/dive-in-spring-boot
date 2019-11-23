8a22f6c0f62e22ff43b4787e5005ca050df4eff7
fengyue <1339148474@qq.com>
2019-11-24 00:04:07 +0800

    更换web server 为  webflux

--- src/main/java/com/imooc/diveinspringboot/web/servlet/AsyncServlet.java
+++ src/main/java/com/imooc/diveinspringboot/web/servlet/AsyncServlet.java
@@ -1,32 +1,32 @@
-package com.imooc.diveinspringboot.web.servlet;
-
-import javax.servlet.AsyncContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-@WebServlet(urlPatterns = "/web/asyncServlet", asyncSupported = true)
-public class AsyncServlet extends HttpServlet {
-
-
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        AsyncContext asyncContext = req.startAsync();
-
-        asyncContext.start(() -> {
-            try {
-                resp.getWriter().println("async servlet demo");
-                asyncContext.complete();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-
-    }
-
-}
+//package com.imooc.diveinspringboot.web.servlet;
+//
+//import javax.servlet.AsyncContext;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//@WebServlet(urlPatterns = "/web/asyncServlet", asyncSupported = true)
+//public class AsyncServlet extends HttpServlet {
+//
+//
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//            throws ServletException, IOException {
+//
+//        AsyncContext asyncContext = req.startAsync();
+//
+//        asyncContext.start(() -> {
+//            try {
+//                resp.getWriter().println("async servlet demo");
+//                asyncContext.complete();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//
+//
+//    }
+//
+//}
--- src/main/java/com/imooc/diveinspringboot/web/servlet/MyServlet.java
+++ src/main/java/com/imooc/diveinspringboot/web/servlet/MyServlet.java
@@ -1,20 +1,20 @@
-package com.imooc.diveinspringboot.web.servlet;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-/**
- * 同步servlet
- */
-@WebServlet(urlPatterns = "/web/my/servlet")
-public class MyServlet extends HttpServlet {
-
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException{
-        resp.getWriter().println("hello world");
-    }
-}
+//package com.imooc.diveinspringboot.web.servlet;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * 同步servlet
+// */
+//@WebServlet(urlPatterns = "/web/my/servlet")
+//public class MyServlet extends HttpServlet {
+//
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+//            throws ServletException, IOException{
+//        resp.getWriter().println("hello world");
+//    }
+//}
